be08c8a45d8ba0ca98b54cd7f28c9edf6d40d207
T-Mo Morris <morenaruto007@gmail.com>
2020-07-03 15:21:56 -0500

    Add Purpose Section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import CarouselSection from "./CarouselSection";
 import CardSection from "./CardSection";
+import PurposeSection from "./PurposeSection";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <CarouselSection/>
+                <PurposeSection/>
                 <CardSection/>
             </div>
         )
